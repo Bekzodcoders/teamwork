4613582b8180a70dcc5a2a0ff288bc55abeebadb
Asilbek <oktamovasilbek12@gmail.com>
2024-02-03 05:17:25 +0500

    While true, Display Menu , show water tugadi. Case 3 qo'shiladi va project yakunlanadi.

--- Entitiy/Addwater.java
+++ Entitiy/Addwater.java
@@ -11,11 +11,18 @@ public class Addwater {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Qancha suv qo'shmoqchisiz");
         int add = scanner.nextInt();
-        if (suv<maxLimt){
-            suv+=add;
+        if (suv < maxLimt) {
+            suv += add;
             System.out.println("Suv qo'shildi");
-        }else {
+        } else {
             System.out.println("Siz qo'shmoqchi bo'lgan suv bochkaga sig'maydi!!!");
         }
+    }
+
+    public static void showWater() {
+        System.out.println("-------------------------------------------");
+        System.out.println("to'la hajmi: " + maxLimt + " litr");
+        System.out.println((maxLimt - suv) + " litr hajmi bo'sh");
+        System.out.println("-------------------------------------------");
     }
 }
--- Main.java
+++ Main.java
@@ -1,8 +1,24 @@
 import Entitiy.Addwater;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Addwater.addwather();
-        System.out.println("Bo'ldi");
+        while (true) {
+            DisplayMenu();
+            switch (new Scanner(System.in).nextInt()) {
+                case 1 -> Addwater.addwather();
+                case 2 -> Addwater.showWater();
+
+            }
+        }
+    }
+
+    private static void DisplayMenu() {
+        System.out.println("""
+                1.add water
+                2.show water
+                3.pour water
+                """);
     }
 }
